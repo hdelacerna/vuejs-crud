76bb9c8797fe1150aed4f2be814257747b1ba336
Harold Dela Cerna <delacerna.harold09@gmail.com>
2025-07-15 21:40:28 +0800

    first commit

---
+++ src/views/auth/Register.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import axiosInstance from '@/lib/axios';
+import { reactive } from 'vue';
+
+interface RegisterForm {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
+
+const form = reactive<RegisterForm>({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+})
+
+const errors = reactive({
+    name:  [],
+    email:  [],
+    password: [],
+})
+
+const register = async (payload : RegisterForm) => {
+    await axiosInstance.get('sanctum/csrf-cookie', {
+        baseURL: 'http://localhost:8000/'
+    });
+    try {
+        const response = await axiosInstance.post('register',payload)
+        console.log(response.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+</script>
+
+<template>
+    <h1 class="text-3xl text-slate-900 p-4">Register</h1>
+
+    <form @submit.prevent="register(form)" class="max-w-sm mx-auto p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div class="mb-5">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="name" id="name" v-model="form.name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name" />
+        </div>
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" v-model="form.email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john@doe.com" />
+        </div>
+        <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+            <input type="password" id="password" v-model="form.password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+        <div class="mb-5">
+            <label for="password_confirmation"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password
+                Confirmation</label>
+            <input type="password" id="password_confirmation" v-model="form.password_confirmation"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+        <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+    </form>
+
+</template>
